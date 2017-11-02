684ac74f186fc674d7843ce36c94cab7ea692826
jschumacher17 <j-schumacher@kellogg.northwestern.edu>
2017-11-02 18:06:25 -0400

    Update

--- Weather/weather.js
+++ Weather/weather.js
@@ -31,19 +31,27 @@ let handleWeatherResponse = function(response) {
   // the response object in the browser console
   console.log(response)
   window.response = response
-//Not part of my code
 
   // **** your code starts here - don't modify anything else. you will be sad.
-  document.getElementById("current-conditions-text").innerHTML = response.currently.temperature;
-  document.getElementById("current-conditions-icon").innerHTML = icon(response.currently);
 
-for(let i=1; i<6; i++) {
+  document.getElementById("current-conditions-text").innerHTML = response.currently.summary;
+  document.getElementById("current-conditions-icon").innerHTML = icon(response.currently);
+  document.getElementById("current-conditions-temp").innerHTML = Math.round(response.currently.temperature);
+  // + "|" + Math.round(response.currently.temperatureMin);
+
+  for(let i=1; i<7; i++) {
+      //document.getElementById("forecast" + i + "-icon").innerHTML = forecast.daily.data[i].icon;
+      //document.getElementById("forecast" + i + "-temp").innerHTML = Math.round(forecast.daily.data[i].temperatureMax);
+      //document.getElementbyId("forecast" + i + "-text").innerHTML = forecast.daily.data[i].summary;
   document.getElementById("forecast" + i + "-icon").innerHTML = icon(response.daily.data[i]);
-  document.getElementById("forecast" + i + "-temp").innerHTML = response.daily.data[i].tempatureHigh;
-  document.getElementbyId("forecast" + i + "-text").innerHTML = response.daily.data[i].summary;
+  document.getElementById("forecast" + i + "-temp").innerHTML = Math.round(response.daily.data[i].temperatureHigh)
+    + "|" + Math.round(response.daily.data[i].temperatureLow);
+  document.getElementById("forecast" + i + "-text").innerHTML = response.daily.data[i].summary;
 }
 // *** your code ends here - no, really.
 };
+
+
 
 // leave this alone; does the event handling and ajax
 $(function() {
